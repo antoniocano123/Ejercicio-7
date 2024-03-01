2eec956261b051a754fab51292289a5aa3adefff
Alumno <Alumno@L13-2543299>
2024-03-01 12:59:04 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace Ejercicio_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbsalida = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            this.lbsalida.AutoSize = true;
+            // 
+            this.lbsalida.Location = new System.Drawing.Point(186, 52);
+            this.lbsalida.Name = "lbsalida";
+            this.lbsalida.Size = new System.Drawing.Size(35, 13);
+            this.lbsalida.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbsalida);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbsalida;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Ejercicio_7
+{
+    public partial class Form1 : Form
+    {
+        int[] precio;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void Imprimir ()
+        {
+            string salida = "";
+            int suma = 0;
+            llenar();
+            double promedio = 0;
+            foreach(int n in precio)
+            {
+                suma += n;
+            }
+            promedio = (double)suma / (double)precio.Length;
+
+
+            foreach (int num in precio)
+            {
+                if (promedio < num)
+                {
+
+                     salida = salida + " "+ $"{num}";
+                }
+                
+            }
+            lbsalida.Text = $"{salida}";
+            
+        }
+
+        public void llenar ()
+        {
+            for (int i = 0 ; i < precio.Length; i++)
+            {
+                Random aleatorio = new Random();
+                int numeroAleatorio = aleatorio.Next(100000, 500000);
+                precio[i] = numeroAleatorio;
+            }
+        }
+    }
+}
